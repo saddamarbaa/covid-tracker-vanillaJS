dc1343df4d15d028378568fb3c311a70c518d5cb
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-02-22 07:05:38 +0700

    ADD:Project Structuret

---
+++ script.js
@@ -0,0 +1,11 @@
+let map;
+
+const initMap = () => {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+  console.log("test");
+};
+
+initMap();
